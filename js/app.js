dbefa1d99d576a627f5bc5e7751928e54c24da60
IvaanGB <igonzale4@cuc.edu.co>
2018-04-06 07:12:40 -0500

    Formulario HTML y JS con validaciones de correo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 enviar=document.getElementById("enviar");
 enviar.onclick=validarDatos;
 
-function validarDatos(){
-	alert("Todo bien todo bien");
+function validarDatos () {
+    var elemento =document.getElementById("correo");
+    if (elemento.checkValidity() ==false && elemento.required){
+    //document.getElementById("mensajes").innerHTML=`<p>Debe ingresar un ${elemento.id} valido</p>`;
+    document.getElementById("mensajes").innerHTML="<p>Debe ingresar un "+elemento.id +" valido</p>";
+}
 }
